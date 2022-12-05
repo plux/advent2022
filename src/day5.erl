6b7bbabc09fa925c6c6bc17ac317537fc2ffb049
Hakan Nilsson <haakan@gmail.com>
2022-12-05 08:50:21 +0100

    Cleanup day 5

--- src/day5.erl
+++ src/day5.erl
@@ -3,48 +3,43 @@
 -include("aoc.hrl").
 
 solve(Input) ->
-    Stacks = case length(?lines(Input)) > 5 of
-                 true ->
-                     {[$P, $Z, $M, $T, $R, $C, $N],
-                      [$Z, $B, $S, $T, $N, $D],
-                      [$G, $T, $C, $F, $R, $Q, $H, $M],
-                      [$Z, $R, $G],
-                      [$H, $R, $N, $Z],
-                      [$D, $L, $Z, $P, $W, $S, $H, $F],
-                      [$M, $G, $C, $R, $Z, $D, $W],
-                      [$Q, $Z, $W, $H, $L, $F, $J, $S],
-                      [$N, $W, $P, $Q, $S]};
-                 false ->
-                     {[$N, $Z],
-                      [$D, $C, $M],
-                      [$P]}
-             end,
-    {part1(?ints_lines(Input), Stacks), part2(?ints_lines(Input), Stacks)}.
+    [Stacks0, Moves0] = ?split(Input, "\n\n"),
+    Stacks = parse_stacks(?lines(Stacks0)),
+    Moves = ?ints_lines(Moves0),
+    {part1(Moves, Stacks), part2(Moves, Stacks)}.
 
 part1(Moves, Stacks) ->
-    top_stacks(eval_moves(Moves, Stacks, fun move/3)).
+    top_crates(eval_moves(Moves, Stacks, fun move/3)).
 
 part2(Moves, Stacks) ->
-    top_stacks(eval_moves(Moves, Stacks, fun move2/3)).
+    top_crates(eval_moves(Moves, Stacks, fun move2/3)).
+
+parse_stacks(Lines) ->
+    {Diagram, Cols} = lists:split(length(Lines)-1, Lines),
+    maps:from_list(
+      [ {N, [lists:nth(N*4 - 2, Line) || Line <- Diagram,
+                                         length(Line) > (N*4 - 2),
+                                         lists:nth(N*4 - 2, Line) >= $A]}
+        || N <- ?ints(Cols) ]).
 
 eval_moves(Moves, Stacks, MoveFun) ->
     lists:foldl(
       fun([N, From, To], Acc) ->
-              {FromL2, ToL2} = MoveFun(N, element(From, Acc), element(To, Acc)),
-              setelement(To, setelement(From, Acc, FromL2), ToL2)
+              {FromS, ToS} = MoveFun(N, maps:get(From, Acc), maps:get(To, Acc)),
+              Acc#{From := FromS, To := ToS}
       end, Stacks, Moves).
 
-move(0, From, To) ->
-    {From, To};
-move(N, [H|From], To) ->
-    move(N-1, From, [H|To]).
+move(N, From, To) ->
+    {Crates, From2} = lists:split(N, From),
+    {From2, lists:reverse(Crates) ++ To}.
 
 move2(N, From, To) ->
-    {Boxes, From2} = lists:split(N, From),
-    {From2, Boxes ++ To}.
+    {Crates, From2} = lists:split(N, From),
+    {From2, Crates ++ To}.
 
-top_stacks(Stacks) ->
-    [hd(S) || S <- tuple_to_list(Stacks)].
+top_crates(Stacks) ->
+    [Crate || {_, [Crate|_]} <- lists:sort(maps:to_list(Stacks))].
+
 
 -include_lib("eunit/include/eunit.hrl").
 solve_test_() ->
