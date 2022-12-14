07b70f206c2be5d63e3cbf3409551e4885f1cf4f
Hakan Nilsson <haakan@gmail.com>
2022-12-14 11:57:33 +0100

    Faster day 14

--- src/day14.erl
+++ src/day14.erl
@@ -7,18 +7,12 @@ solve(Input) ->
     {part(Grid, 1), part(Grid, 2)}.
 
 parse(Input) ->
-    to_grid(?ints_lines(Input), #{}).
-
-to_grid([], Grid) ->
-    Grid;
-to_grid([Line|Rest], Grid) ->
-     to_grid(Rest,
-             maps:merge(Grid, maps:from_list(lists:flatten(to_line(Line))))).
+    maps:from_list(lists:flatten([to_line(L) || L <- ?ints_lines(Input)])).
 
 to_line([_,_]) ->
     [];
-to_line([FromX, FromY, ToX, ToY|Rest]) ->
-    [to_line(FromX, FromY, ToX, ToY)|to_line([ToX, ToY|Rest])].
+to_line([FromX, FromY | [ToX, ToY | _] = Rest]) ->
+    [to_line(FromX, FromY, ToX, ToY) | to_line(Rest)].
 
 to_line(X, FromY, X, ToY) when FromY < ToY ->
     [{{X, Y}, $#} || Y <- lists:seq(FromY, ToY)];
@@ -29,29 +23,30 @@ to_line(FromX, Y, ToX, Y) when FromX < ToX ->
 to_line(FromX, Y, ToX, Y) ->
     [{{X, Y}, $#} || X <- lists:seq(ToX, FromX)].
 
+-define(start, {500, 0}).
 part(Grid, Part) ->
-    Start = {500, 0},
     Limit = lists:max([Y || {_, Y} <- maps:keys(Grid)]),
-    Grid2 = drop_sand(Start, Grid, Limit, Part),
-    length([1 || $o <- maps:values(Grid2)]).
+    length([1 || $o <- maps:values(drop_sand(?start, [], Grid, Limit, Part))]).
 
-drop_sand({_X, Y}, Grid, Limit, Part) when Y == Limit, Part == 1 ->
+drop_sand({_X, Y}, _Prev, Grid, Limit, _Part = 1) when Y == Limit ->
     Grid;
-drop_sand({X, Y}, Grid, Limit, Part) when Y+1 == Limit+2, Part == 2 ->
-    drop_sand({500, 0}, Grid#{{X, Y} => $o}, Limit, Part);
-drop_sand({X, Y}, Grid, Limit, Part) ->
+drop_sand({X, Y}, Prev, Grid, Limit, Part = 2) when Y == Limit+1 ->
+    drop_sand(hd(Prev), tl(Prev), Grid#{{X, Y} => $o}, Limit, Part);
+drop_sand({X, Y}, Prev, Grid, Limit, Part) ->
     Points = [{X, Y}, {X-1, Y+1}, {X, Y+1}, {X+1, Y+1}],
     case [maps:is_key(P, Grid) || P <- Points] of
         [true, _, _, _] ->
             Grid;
+        [_, true, true, true] when Prev == [] ->
+            drop_sand(?start, [], Grid#{{X, Y} => $o}, Limit, Part);
         [_, true, true, true] ->
-            drop_sand({500, 0}, Grid#{{X, Y} => $o}, Limit, Part);
+            drop_sand(hd(Prev), tl(Prev), Grid#{{X, Y} => $o}, Limit, Part);
         [_, true, true, _] ->
-            drop_sand({X+1, Y+1}, Grid, Limit, Part);
+            drop_sand({X+1, Y+1}, [{X, Y}|Prev], Grid, Limit, Part);
         [_, _, true, _] ->
-            drop_sand({X-1, Y+1}, Grid, Limit, Part);
+            drop_sand({X-1, Y+1}, [{X, Y}|Prev], Grid, Limit, Part);
         [_, _, _, _] ->
-            drop_sand({X, Y+1}, Grid, Limit, Part)
+            drop_sand({X, Y+1}, [{X, Y}|Prev], Grid, Limit, Part)
     end.
 
 -include_lib("eunit/include/eunit.hrl").
